6a6682544d7a1f558d6357ba276b7a239e8e562c
atomd <dwb319@gmail.com>
2013-03-25 13:47:33 +0800

    Update

--- forest/paths.py
+++ forest/paths.py
@@ -16,35 +16,36 @@ path_opts = [
                help="Top-level directory for maintaining forest's state"),
 ]
 
+
 CONF = cfg.CONF
 CONF.register_opts(path_opts)
 
 
 def basedir_def(*args):
-    """Return an uninterpolated path relative to $pybasedir."""
+    ''' Return an uninterpolated path relative to $pybasedir. '''
     return path.join('$pybasedir', *args)
 
 
 def bindir_def(*args):
-    """Return an uninterpolated path relative to $bindir."""
+    ''' Return an uninterpolated path relative to $bindir. '''
     return path.join('$bindir', *args)
 
 
 def state_path_def(*args):
-    """Return an uninterpolated path relative to $state_path."""
+    ''' Return an uninterpolated path relative to $state_path. '''
     return path.join('$state_path', *args)
 
 
 def basedir_rel(*args):
-    """Return a path relative to $pybasedir."""
+    ''' Return a path relative to $pybasedir. '''
     return path.join(CONF.pybasedir, *args)
 
 
 def bindir_rel(*args):
-    """Return a path relative to $bindir."""
+    ''' Return a path relative to $bindir. '''
     return path.join(CONF.bindir, *args)
 
 
 def state_path_rel(*args):
-    """Return a path relative to $state_path."""
+    ''' Return a path relative to $state_path. '''
     return path.join(CONF.state_path, *args)
