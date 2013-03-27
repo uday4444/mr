4448d47704f694c3afe6df8e778b456435663418
atomd <dwb319@gmail.com>
2013-03-27 22:30:22 +0800

    Update

--- forest/config.py
+++ forest/config.py
@@ -12,21 +12,24 @@ _DEFAULT_SQL_CONNECTION = 'sqlite:///' + paths.state_path_def('$sqlite_db')
 
 
 def parse_args(argv, default_config_files=None):
-    # Default sqlite database config
-    db_session.set_defaults(sql_connection=_DEFAULT_SQL_CONNECTION,
-                            sqlite_db='forest.sqlite')
+    # Default sqlite database config #  FIXME
+    # FIXME The api is wrong
+    cfg.set_defaults(db_session.sql_opts,
+                     sql_connection=_DEFAULT_SQL_CONNECTION)
+    cfg.set_defaults(db_session.sql_opts, sqlite_db='forest.sqlite')
     # Default rabbitmq control_exchange config
     rpc.set_defaults(control_exchange='forest')
     # Generate the common config
-    cfg.CONF(argv[1:], project='forest',
+    cfg.CONF(argv[1:],
+             project='forest',
              default_config_files=default_config_files)
 
 
 # TODO Simple test, REMOVE!
 if __name__ == '__main__':
     import sys
-    import pprint
+    #import pprint
     parse_args(sys.argv)
-    pprint.pprint(cfg.CONF.__dict__)
+    #pprint.pprint(cfg.CONF.__dict__)
     print ''
     print cfg.CONF.sql_connection
