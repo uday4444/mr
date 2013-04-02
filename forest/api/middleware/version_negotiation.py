f3b84a65131ccf9710f37786353870c8ff4ab5a5
atomd <dwb319@gmail.com>
2013-04-02 21:56:17 +0800

    Add version filter

---
+++ forest/api/middleware/version_negotiation.py
@@ -0,0 +1,62 @@
+'''
+A filter middleware that inspects the requested URI for a version string
+and/or Accept headers and attempts to negotiate an API controller to
+return
+'''
+
+from oslo.config import cfg
+
+import openstack.common.wsgi
+import openstack.common.log as logging
+
+from forest.api import versions
+
+CONF = cfg.CONF
+LOG = logging.getLogger(__name__)
+
+
+class VersionNegotiationFilter(openstack.common.wsgi.Middleware):
+
+    def __init__(self, app):
+        self.versions_app = versions.Controller()
+        super(VersionNegotiationFilter, self).__init__(app)
+
+    def process_request(self, req):
+        ''' Try to find a version first in the accept header, then the URL '''
+        msg = ("Determining version of request: %(method)s %(path)s"
+               " Accept: %(accept)s")
+        args = {'method': req.method, 'path': req.path, 'accept': req.accept}
+        LOG.debug(msg % args)
+
+        if req.path_info_peek() == "versions":
+            return self.versions_app
+
+
+        try:
+            # Remove version in url so it doesn't conflict later
+            req_version = req.path_info_pop()
+            version = self._match_version_string(req_version)
+        except ValueError:
+            LOG.debug("Unknown version. Returning version choices.")
+            return self.versions_app
+
+        req.environ['api.version'] = version
+        req.path_info = ''.join(('/v', str(version), req.path_info))
+        LOG.debug("Matched version: v%d", version)
+        LOG.debug('new uri %s' % req.path_info)
+        return None
+
+    def _match_version_string(self, subject):
+        '''
+        Given a string, tries to match a major and/or
+        minor version number.
+
+        :param subject: The string to check
+        :returns version found in the subject
+        :raises ValueError if no acceptable version could be found
+        '''
+        if subject in ('v1', 'v1.0'):
+            major_version = 1
+        else:
+            raise ValueError()
+        return major_version
---
+++ forest/api/versions.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+import httplib
+import json
+import webob.dec
+
+class Controller(object):
+    ''' A controller that produces information on the heat API versions. '''
+
+    def __init__(self, conf):
+        self.conf = conf
+
+    @webob.dec.wsgify
+    def __call__(self, req):
+        ''' Respond to a request for all OpenStack API versions. '''
+        version_objs = [{
+            'id': 'v1.0',
+            'status': 'CURRENT',
+            'links': [{
+                'rel': 'self',
+                'href': self.get_href(req)
+            }]
+        }]
+
+        response = webob.Response(request=req,
+                                  status=httplib.MULTIPLE_CHOICES,
+                                  content_type='application/json')
+        response.body = json.dumps(dict(versions=version_objs))
+        return response
+
+    def get_href(self, req):
+        return "%s/v1/" % req.host_url
